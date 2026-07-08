df47e24cf10f74d85df5aa58c700c4bef1f57c92
Michael Konstantinou <mikhailkonstan@hotmail.com>
2026-07-08 13:23:02 +0200

    Updated: generic heading components

--- resources/js/components/Heading.vue
+++ resources/js/components/Heading.vue
@@ -14,7 +14,7 @@ defineProps<Props>();
 
 <template>
     <div class="mb-8 space-y-0.5">
-        <h2 class="text-xl font-semibold tracking-tight">{{ $t(title, titleParams) }}</h2>
+        <h2 class="text-2xl font-semibold tracking-tight">{{ $t(title, titleParams) }}</h2>
         <p v-if="description" class="text-sm text-muted-foreground">
             {{ $t(description) }}
         </p>
--- resources/js/components/HeadingSmall.vue
+++ resources/js/components/HeadingSmall.vue
@@ -8,8 +8,8 @@ defineProps<Props>();
 </script>
 
 <template>
-    <header>
-        <h3 class="mb-0.5 text-base font-medium">{{ $t(title) }}</h3>
+    <header class="mb-5">
+        <h3 class="mb-0.5 text-2xl font-medium">{{ $t(title) }}</h3>
         <p v-if="description" class="text-sm text-muted-foreground">
             {{ $t(description) }}
         </p>
